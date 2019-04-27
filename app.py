d631652e063017a8a7d96dee626a3c77d9bc158e
ghigbie <gehigbie@gmail.com>
2019-04-27 09:38:38 -0600

    Add views/routes

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ import stripe
 app = Flask(__name__)
 
 public_key = 'pk_test_6pRNASCoBOKtIshFeQd4XMUh'
-
 stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', public_key=public_key)
+
+@app.route('/thankyou')
+def index():
+    return render_template('thankyou.html')
