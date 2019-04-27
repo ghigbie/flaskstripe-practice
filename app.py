50698865d37578eb185fc598f404a02e06deb750
ghigbie <gehigbie@gmail.com>
2019-04-27 09:26:59 -0600

    Add basic flask imports

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, redirect, url_for
+import stripe 
+
+app = Flask(__name__)
+
+public_key = 'pk_test_6pRNASCoBOKtIshFeQd4XMUh'
+
+stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
